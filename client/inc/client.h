c695c1985632e1c9e483d13148c5b5700ecc7065
Xiaodiana0308 <wwei04091@gmail.com>
2022-03-21 15:42:52 +0800

    first update

---
+++ client/inc/client.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <assert.h>
+
